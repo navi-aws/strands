424fa298711b13206e0854befcc96fd2990a23fd
Pro <47291803+navi-aws@users.noreply.github.com>
2025-10-04 19:48:29 +1000

    Update exercise-1.py
    changed model

--- exercise-1.py
+++ exercise-1.py
@@ -1,11 +1,12 @@
 from strands import Agent
 from strands.models import BedrockModel
 
-model = BedrockModel(model_id="us.amazon.nova-pro-v1:0",
+model = BedrockModel(model_id="amazon.nova-lite-v1:0",
                    streaming=True)
 
 # Create an agent with default settings
 agent = Agent(model=model)
 
 # Ask the agent a question
+
 agent("Tell me about India in 3 sentences")
