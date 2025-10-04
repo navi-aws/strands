cdb9a35dd75fdf5a76f04b362bc26ab269cf50c8
Pro <47291803+navi-aws@users.noreply.github.com>
2025-10-04 19:54:21 +1000

    Update exercise-5.py

--- exercise-5.py
+++ exercise-5.py
@@ -1,11 +1,14 @@
 from strands import Agent
 from strands_tools import http_request
+from strands.models import BedrockModel
+
+model = BedrockModel(model_id="amazon.nova-lite-v1:0"
+                   )
 
 # Create a basic agent with a specialized system prompt
-dog_breed_helper = Agent(
-    system_prompt="""You are a dog breed expert.     
-    Your goal is to help pet parents make an informed decision about their choice in a dog.
-    in bullet points. No explanation required.
+dog_info = Agent(model=model,
+    system_prompt="""You are a dog expert.     
+    Your goal is to help get information about dogs in bullet points. No explanation required.
     """,
     tools=[http_request]
 )
@@ -15,5 +18,5 @@ Answer this question:
 Search wikipedia for the top 5 most popular dog breeds of 2024.
 """
 # Run the agent with the query
-response = dog_breed_helper(query)
+response = dog_info(query)
 
