5be24803ca0bd456ce5e78f3a7f7cf7dea948baf
Pro <47291803+navi-aws@users.noreply.github.com>
2025-11-21 16:42:48 +1030

    Update exercise-5.py

--- exercise-5.py
+++ exercise-5.py
@@ -1,16 +1,24 @@
 from strands import Agent
-from strands_tools import http_request
+from ddgs import DDGS
 from strands.models import BedrockModel
 
 model = BedrockModel(model_id="amazon.nova-lite-v1:0"
                    )
 
-# Create a basic agent with a specialized system prompt
+# Create a web search tool
+@tool
+def websearch(keywords: str, region: str = "us-en", max_results: int = 1) -> str:
+    try:
+        results = DDGS().text(keywords, region=region, max_results=max_results)
+        return results if results else "No results found."
+    except Exception as e:
+        return f"Error: {e}"
+        # Create a basic agent with a specialized system prompt
 dog_info = Agent(model=model,
     system_prompt="""You are a dog expert.     
     Your goal is to help get information about dogs in bullet points. No explanation required.
     """,
-    tools=[http_request]
+    tools=[websearch]
 )
 
 query = """
@@ -19,4 +27,3 @@ Search wikipedia for the top 5 most popular dog breeds of 2024.
 """
 # Run the agent with the query
 response = dog_info(query)
-
